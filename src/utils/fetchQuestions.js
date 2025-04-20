4b85541787f18406b359c756917aab511d7a5862
Rinshika Dubey <rinshikadubey@gmail.com>
2025-04-21 02:06:44 +0530

    Fixed questions array parsing

--- src/utils/fetchQuestions.js
+++ src/utils/fetchQuestions.js
@@ -1,7 +1,9 @@
+
 const fetchQuestions = async () => {
     try {
-      const res = await fetch('http://localhost:3001/questions');
-      return await res.json();
+      const res = await fetch('/questions.json');
+      const data = await res.json();
+      return data.questions || []; // extract questions array
     } catch (error) {
       console.error('Fetch error:', error);
       return [];
